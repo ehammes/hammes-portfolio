e03687ba4fd83115488dc4f9fc361a365d6ad1ba
Elizabeth <elisha.elizabeth@gmail.com>
2023-09-14 22:28:21 -0700

    Configured Footer layout and copyright

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,15 @@
-
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+  const year = new Date().getFullYear();
 
-export default Footer
+  return (
+    <div id='footer' className='bg-white'>
+      <div
+        className='pb-5 mx-3 pt-5 text-slate-500 lg:text-sm text-xs text-right'
+      >
+        <div class='mt-2'>© Copyright {year}. All Rights Reserved</div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
