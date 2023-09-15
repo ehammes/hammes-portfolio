83f661d45558c2cc20a8ad798db01b0085cf6883
Elizabeth <elisha.elizabeth@gmail.com>
2023-09-14 22:26:03 -0700

    Updated styles

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import Header from './components/Header';
+// import Header from './components/Header';
 import Hero from './components/Hero';
 import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
+// import { Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className=''>
+    <div className='bg-slate-50'>
       <Header />
       <Hero />
       <About />
       <Projects />
       <Contact />
+      <Footer />
     </div>
   );
 }
