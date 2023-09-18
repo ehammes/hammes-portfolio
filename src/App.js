6ec529943d7aead0fb8c93d265792e6dae5f315c
Elizabeth <elisha.elizabeth@gmail.com>
2023-09-17 22:32:31 -0700

    Added high level div for main content and adjusted styles

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-// import Header from './components/Header';
+import Header from './components/Header';
 import Hero from './components/Hero';
 import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
-// import { Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
 import Footer from './components/Footer';
 
 function App() {
   return (
     <div className='bg-slate-50'>
       <Header />
-      <Hero />
-      <About />
-      <Projects />
-      <Contact />
+      <div className='mx-6 mt-12 md:mt-36 md:mb-12'>
+        <Hero />
+        <About />
+        <Projects />
+        <Contact />
+      </div>
       <Footer />
     </div>
   );
