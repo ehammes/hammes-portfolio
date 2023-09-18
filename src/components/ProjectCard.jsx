daad5fe88952bccc07c393f4e3d5bf2d87759c34
Elizabeth <elisha.elizabeth@gmail.com>
2023-09-17 22:36:03 -0700

    Updated styles for responsive project cards

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -13,7 +13,7 @@ import projectData from './projectData';
 
 const ProjectCard = () => {
   return (
-    <div className='grid grid-cols-1 py-6 mx-auto items-top md:grid-cols-2 md:py-12'>
+    <div className='grid grid-cols-1 mx-auto mt-6 items-top md:grid-cols-2'>
       {projectData.map((project) => {
         return (
           <Card key={project.id} className='p-6 m-6 bg-white border shadow-md'>
@@ -23,26 +23,26 @@ const ProjectCard = () => {
               </Heading>
             </CardHeader>
             <CardBody>
-              <Image
-                src={project.imageUrl}
-                alt='project image'
-                className='mx-auto border-2 rounded-lg max-h-48 border-slate-200'
-              />
+                <Image
+                  src={project.imageUrl}
+                  alt='project image'
+                  className='mx-auto border-2 rounded-lg max-h-44 max-w-64 border-slate-200'
+                />
               <Stack mt='6' spacing='3'>
                 <Text fontSize='medium' className='py-2'>
                   {project.description}
                 </Text>
                 <div>
-                  <div className='font-semibold'>TECH STACK:</div>
+                  <Text className='font-semibold'>TECH STACK:</Text>
                   {project.tech}
                 </div>
               </Stack>
             </CardBody>
             <hr className='h-px my-6 bg-gray-200 border-0 dark:bg-slate-700' />
-            <CardFooter className='mx-auto'>
-              <ButtonGroup className='items-center space-x-12 md:space-x-24'>
+            <CardFooter className='mx-auto my-auto'>
+              <ButtonGroup className='items-center space-x-6 lg:space-x-24 md:space-x-12'>
                 <a
-                  className='rounded-md bg-blue-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600'
+                  className='rounded-md bg-blue-600 lg:px-3.5 lg:py-2.5 p-2.5 text-sm md:px-2.5 md:py-2.5 lg:text-base md:text-base font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600'
                   href={project.siteUrl}
                   target='_blank'
                   rel='noreferrer'
@@ -51,7 +51,7 @@ const ProjectCard = () => {
                 </a>
                 <a
                   variant='ghost'
-                  className='font-semibold leading-6 text-gray-900 text-md hover:underline'
+                  className='text-sm font-semibold leading-6 text-gray-900 md:text-base hover:underline'
                   href={project.gitHubUrl}
                   target='_blank'
                   rel='noreferrer'
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,8 +3,7 @@ import ProjectCard from './ProjectCard';
 const Projects = () => {
   return (
     // PROJECTS SECTION
-    <div id='projects'>
-      <div className='px-6 mt-6 bg-slate-50 '>
+    <div id='projects' className='pb-16 bg-slate-50'>
         <div className='text-center'>
           <h1 className='text-2xl font-bold tracking-tight text-gray-900 md:text-4xl'>
             PROJECTS
@@ -13,7 +12,6 @@ const Projects = () => {
 
         {/* PROJECT CARDS */}
         <ProjectCard />
-      </div>
     </div>
   );
 };
